308fe22c3c786f93e14061511d6885528d1ae4b6
Aniruddha <bajpaianirud03@gmail.com>
2022-07-22 17:17:10 +0530

    adding changes

---
+++ JS/script.js
@@ -0,0 +1,28 @@
+// const { default: Typed } = require("typed.js");
+
+// var typed = new Typed(".t1", {
+//   strings: [
+//     "The heaven for bloggers",
+//     // "Place for blogging",
+//     // "Write your first blog",
+//   ],
+//   backSpeed: 100,
+//   // backDelay: 5,
+//   typeSpeed: 100,
+
+//   // loop: true,
+// });
+var typed = new Typed(".t2", {
+  strings: [
+    "Heaven for bloggers",
+    "Write what you know.",
+    "Content is anything that adds value to the reader's life.",
+    "Any idea is only as valuable as its ability to solve a problem.",
+    "Don't focus on having a great blog. Focus on producing a blog that's great for your readers.",
+    "Either write something worth reading or do something worth writing.",
+  ],
+  backSpeed: 80,
+  // backDelay: 5,
+  typeSpeed: 80,
+  loop: true,
+});
